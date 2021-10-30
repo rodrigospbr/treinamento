7d028ca6e234753a0bf4829e4a60778e36690ea1
rodrigospbr <coutinho2006@yahoo.com.br>
2021-10-30 15:52:27 -0300

    Primeiro comit, classes Pedido e StatusPedido

---
+++ Pedido.java
@@ -0,0 +1,82 @@
+package com.br.spring.mvc.mudi.model;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "pedidos")
+public class Pedido {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nomeProduto;
+	private BigDecimal valorNegociado;
+	private LocalDate dataEntrega;
+	private String urlProduto;
+	private String urlImagem;
+	private String descricao;
+	
+	@Enumerated(EnumType.STRING)
+	private StatusPedido status;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNomeProduto() {
+		return nomeProduto;
+	}
+	public void setNomeProduto(String nomeProduto) {
+		this.nomeProduto = nomeProduto;
+	}
+	public BigDecimal getValorNegociado() {
+		return valorNegociado;
+	}
+	public void setValorNegociado(BigDecimal valorNegociado) {
+		this.valorNegociado = valorNegociado;
+	}
+	public LocalDate getDataEntrega() {
+		return dataEntrega;
+	}
+	public void setDataEntrega(LocalDate dataEntrega) {
+		this.dataEntrega = dataEntrega;
+	}
+	public String getUrlProduto() {
+		return urlProduto;
+	}
+	public void setUrlProduto(String urlProduto) {
+		this.urlProduto = urlProduto;
+	}
+	public String getUrlImagem() {
+		return urlImagem;
+	}
+	public void setUrlImagem(String urlImagem) {
+		this.urlImagem = urlImagem;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public StatusPedido getStatus() {
+		return status;
+	}
+	public void setStatus(StatusPedido status) {
+		this.status = status;
+	}
+	
+
+}
---
+++ StatusPedido.java
@@ -0,0 +1,6 @@
+package com.br.spring.mvc.mudi.model;
+
+public enum StatusPedido {
+	AGUARDANDO, APROVADO, ENTREGUE
+
+}
