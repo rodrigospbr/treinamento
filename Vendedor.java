f1a110aa38c3fd261ba29e637c2cb9de109c8b34
DANIELA <coutinho2006@yahoo.com.br>
2021-10-30 22:54:32 -0300

    Método get e set Vendeor.java

--- Vendedor.java
+++ Vendedor.java
@@ -2,6 +2,12 @@ public class Vendedor{
     private Integer idVendedor;
     private String nomeVendedor;
 
-    
+    public void setIdVendedor(Integer idVendedor){
+        this.idVendedor = idVendedor;
+    }
+
+    public Integer getIndVendedor(){
+        return idVendedor;
+    }   
 
 }
